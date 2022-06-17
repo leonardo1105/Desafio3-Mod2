77d115dc428e5ea2d6e7e181aabf5f3a2a2d3aaf
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-06-17 12:21:01 -0400

    Desafio Finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Contagem de 1 a 10")
+for(let num=1;num<=10;num++){
+    console.log(num)
+}
+console.log('Contagem de 10 a 1')
+num=10
+while(num>=1){
+    console.log(num)
+    num--
+}
+console.log('Contagem 1 a 100 em ímpares')
+for(num=1;num<=100;num+=2){
+    console.log(num)
+}
+console.log('Contagem de 0 a 100 em pares')
+for(num=0;num<=100;num+=2){
+    console.log(num)
+}
